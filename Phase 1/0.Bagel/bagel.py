ca911447ca3674811aa647cbfda38e41e6b959d7
0d3ss3y <samuelkhoali002@gmail.com>
2024-10-31 19:28:44 +0200

    Rules and Regulations Complete

--- Phase 1/0.Bagel/bagel.py
+++ Phase 1/0.Bagel/bagel.py
@@ -1,8 +1,31 @@
+import random
+
 def main():
-    pass
+    heading()
+    rules()
+    number = generate_random_number()
+    play_game(number)
+
+
+
+
+
+def generate_random_number():
+    return random.randint(1, 999)
+
+def rules():
+    print("""
+I am thinking of a 3-digit number. Try to guess what it is.
+
+Here are some clues:
+When I say: That means:
+ Pico One digit is correct but in the wrong position.
+ Fermi One digit is correct and in the right position.
+ Bagels No digit is correct.
+    """)
 
 def heading():
-    print("Bagels, a deductive logic game.\n")
+    print("Bagels, a deductive logic game.")
 
 
 if __name__ == '__main__':
