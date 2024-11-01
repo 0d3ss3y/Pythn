3467ac8e7e8d5e19592c2e6de6e63f2f20de13c1
0d3ss3y <samuelkhoali002@gmail.com>
2024-11-01 23:52:26 +0200

    done

--- Phase 1/1.BirthdayParadox/birthday.py
+++ Phase 1/1.BirthdayParadox/birthday.py
@@ -1,12 +1,11 @@
 import numpy as np
-import random
-import datetime as dt
 
 def main():
     heading()
     limit = get_limit()
-    birthdays = generate_birthdays(limit)
-    display_birthdays(birthdays)
+    simulation_count = 100_000
+    matching_count = run_sim(limit, simulation_count)
+    display_results(limit, simulation_count, matching_count)
 
 def heading():
     print("Birthday Paradox")
@@ -15,27 +14,34 @@ def get_limit():
     print("\nHow many birthdays shall I generate? (Max 100)")
     num = input("> ")
 
-    if num.isdigit() and int(num) < 100:
+    if num.isdigit() and 1 <= int(num) <= 100:
         return int(num)
     else:
-        print("Please enter a valid number.")
+        print("Please enter a valid number between 1 and 100.")
         return get_limit()
 
-def generate_birthdays(limit):
-    startOfYear = dt.date(2024,1,1)
-    birthdays = []
+def run_sim(limit, simulation_count):
+    matching_count = 0
 
-    for i in range(limit):
-        birthday = startOfYear + dt.timedelta(days=random.randint(1,365))
-        birthdays.append(birthday)
-    return birthdays
+    for i in range(simulation_count):
+        birthdays = np.random.randint(0,365, size=limit)
 
-def display_birthdays(birthdays):
-    print(f"\nHere are {len(birthdays)} birthdays:")
-    for birthday in birthdays:
-        month = dt.datetime.strftime(birthday, "%b")
-        day = birthday.day
-        print(f"{month} {day}", end=", ")
+        if len(np.unique(birthdays)) < limit:
+            matching_count += 1
+
+        if (i + 1) % 10_000 == 0:
+            print(f"{i + 1} simulations run")
+
+    return matching_count
+
+def display_results(limit, simulation_count, matching_count):
+    probability = (matching_count / simulation_count) * 100
+
+    print(f"\nOut of {simulation_count} simulations of {limit} people, there was a matching birthday "
+          f"in that group {matching_count} times.")
+    print(f"This means that {limit} people have a {probability:.2f}% chance of having a matching "
+          f"birthday in their group.")
+    print("That's probably more than you would think!")
 
 if __name__ == '__main__':
     main()
